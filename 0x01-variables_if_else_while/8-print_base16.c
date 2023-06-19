e90ffcd5445a312500d856437b78fce88e80cd09
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-18 22:38:15 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	/* Print numbers 0-9 */
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
+
+	/* Print lowercase letters a-f */
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return 0;
+}
