d1898bc2cceee8ac07b3b396272ba3a2ad5e2042
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-20 03:02:36 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+
+    putchar('\n');
+}
+
+int main(void)
+{
+    print_alphabet();
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);  // Prototype of the _putchar function
+
+#endif /* MAIN_H */
