4c6a8cfbe8c0728c4ddecbf3b8b60963df5639c5
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-22 03:03:28 -0700

    All

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char ibrahim = 'a';
+
+	while (ibrahim <= 'z')
+	{
+		_putchar(ibrahim);
+		ibrahim++;
+	}
+
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,4 +5,3 @@ int _putchar(char c)
 {
 	return write(1, &c, 1);
 }
-
