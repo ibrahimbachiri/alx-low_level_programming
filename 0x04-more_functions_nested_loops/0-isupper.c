f6e34a2b1a91f79dc1fcca4799572cf862b1c7ac
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-22 09:43:25 -0700

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,15 @@
 #include <unistd.h>
 
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
+ *
+ * Description: Checks if the given character is an uppercase letter
+ *              according to the ASCII values.
+ *
+ * Return: 1 if `c` is uppercase, 0 otherwise
+ */
+
 int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
