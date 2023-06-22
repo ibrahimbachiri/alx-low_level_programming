af4b1e4b16af27714ede16f26cda868d656d955d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-22 04:46:38 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - check the code
@@ -11,8 +11,8 @@ int main(void)
     char c;
 
     c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
+    putchar("%c: %d\n", c, _isdigit(c));
     c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+    putchar("%c: %d\n", c, _isdigit(c));
     return (0);
 }
