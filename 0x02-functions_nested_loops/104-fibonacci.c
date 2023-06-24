3e15d2b61edce5231ce1067bd2157724f7e3ea1e
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-23 23:18:57 -0700

    m

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,17 @@
-k#include <stdio.h>
+#include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
 int main(void)
 {
     unsigned long int num1 = 1;
     unsigned long int num2 = 2;
     unsigned long int nextNum;
-    int count;
 
-    printf("%lu, %lu, ", num1, num2); /* Print the first two Fibonacci numbers */
+    printf("%lu, %lu, ", num1, num2);
 
-    for (count = 3; count <= 98; count++)
+    while (num1 + num2 < 4000000)
     {
         nextNum = num1 + num2;
-        printf("%lu", nextNum);
-
-        if (count < 98)
-            printf(", ");
+        printf("%lu, ", nextNum);
 
         num1 = num2;
         num2 = nextNum;
@@ -28,5 +19,5 @@ int main(void)
 
     printf("\n");
 
-    return (0);
+    return 0;
 }
