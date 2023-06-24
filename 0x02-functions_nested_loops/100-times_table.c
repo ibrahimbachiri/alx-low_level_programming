2db14363017830e38c05f9b8a8638e163fbf5a33
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-23 22:19:37 -0700

    m

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_times_table - Prints the n times table starting from 0.
+ * @n: The number of times tables to print.
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return;
+
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
+
+			if (j > 0)
+				_putchar(',');
+
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
