ab376ecb3b8af49018266aee5ec184d2ee368cac
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-23 22:40:33 -0700

    m

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of the program
@@ -7,23 +7,28 @@
  */
 int main(void)
 {
-	int i;
-	long int fib1 = 1; // First Fibonacci number
-	long int fib2 = 2; // Second Fibonacci number
+    int i;
+    long int fib1 = 1; /* First Fibonacci number */
+    long int fib2 = 2; /* Second Fibonacci number */
+    char comma[] = ", ";
+    char newline[] = "\n";
 
-	// Print the first two Fibonacci numbers
-	printf("%ld, %ld", fib1, fib2);
+    /* Print the first two Fibonacci numbers */
+    write(STDOUT_FILENO, &fib1, sizeof(fib1));
+    write(STDOUT_FILENO, comma, sizeof(comma) - 1);
+    write(STDOUT_FILENO, &fib2, sizeof(fib2));
 
-	// Generate and print the remaining Fibonacci numbers
-	for (i = 3; i <= 50; i++)
-	{
-		long int nextFib = fib1 + fib2;
-		printf(", %ld", nextFib);
-		fib1 = fib2;
-		fib2 = nextFib;
-	}
+    /* Generate and print the remaining Fibonacci numbers */
+    for (i = 3; i <= 50; i++)
+    {
+        long int nextFib = fib1 + fib2;
+        write(STDOUT_FILENO, comma, sizeof(comma) - 1);
+        write(STDOUT_FILENO, &nextFib, sizeof(nextFib));
+        fib1 = fib2;
+        fib2 = nextFib;
+    }
 
-	printf("\n");
+    write(STDOUT_FILENO, newline, sizeof(newline) - 1);
 
-	return (0);
+    return 0;
 }
