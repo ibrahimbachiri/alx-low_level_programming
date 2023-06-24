9a7c376328ab7bfea076be9d6161385875ec9978
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-23 22:45:39 -0700

    m

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point of the program
@@ -10,25 +10,20 @@ int main(void)
     int i;
     long int fib1 = 1; /* First Fibonacci number */
     long int fib2 = 2; /* Second Fibonacci number */
-    char comma[] = ", ";
-    char newline[] = "\n";
 
     /* Print the first two Fibonacci numbers */
-    write(STDOUT_FILENO, &fib1, sizeof(fib1));
-    write(STDOUT_FILENO, comma, sizeof(comma) - 1);
-    write(STDOUT_FILENO, &fib2, sizeof(fib2));
+    printf("%ld, %ld", fib1, fib2);
 
     /* Generate and print the remaining Fibonacci numbers */
     for (i = 3; i <= 50; i++)
     {
         long int nextFib = fib1 + fib2;
-        write(STDOUT_FILENO, comma, sizeof(comma) - 1);
-        write(STDOUT_FILENO, &nextFib, sizeof(nextFib));
+        printf(", %ld", nextFib);
         fib1 = fib2;
         fib2 = nextFib;
     }
 
-    write(STDOUT_FILENO, newline, sizeof(newline) - 1);
+    printf("\n");
 
     return (0);
 }
