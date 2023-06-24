69939ce3a9df100476b69d2a99eeb1a2c780774a
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-24 00:36:34 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,25 @@
  */
 int main(void)
 {
-    int digit1, digit2;
+	int digit1;
+	int digit2;
 
-    for (digit1 = 0; digit1 <= 8; digit1++)
-    {
-        for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
-        {
-            write(1, &digit1, 1);
-            write(1, &digit2, 1);
+	for (digit1 = 0; digit1 <= 8; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		{
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
 
-            if (digit1 != 8 || digit2 != 9)
-            {
-                write(1, ", ", 2);
-            }
-        }
-    }
+			if (!(digit1 == 8 && digit2 == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    write(1, "\n", 1);
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
