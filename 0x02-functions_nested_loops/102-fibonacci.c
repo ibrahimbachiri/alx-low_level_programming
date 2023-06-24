752b5d9eca9e436c210333f89854acd4c54a78ae
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-23 22:38:15 -0700

    m

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	long int fib1 = 1; // First Fibonacci number
+	long int fib2 = 2; // Second Fibonacci number
+
+	// Print the first two Fibonacci numbers
+	printf("%ld, %ld", fib1, fib2);
+
+	// Generate and print the remaining Fibonacci numbers
+	for (i = 3; i <= 50; i++)
+	{
+		long int nextFib = fib1 + fib2;
+		printf(", %ld", nextFib);
+		fib1 = fib2;
+		fib2 = nextFib;
+	}
+
+	printf("\n");
+
+	return (0);
+}
