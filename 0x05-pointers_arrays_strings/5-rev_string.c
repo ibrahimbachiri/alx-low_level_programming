80afb39248a4cdf35a881082481e553c772aafa6
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-26 02:59:58 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (s[j] != '\0')
+		j++;
+
+	j--;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@
  * reset_to_98 - Updates the value of an integer to 98
  * @n: Pointer to the integer to be updated
  */
+
+void rev_string(char *s);
 void print_rev(char *s);
 void _puts(char *str);
 int _strlen(char *s);
