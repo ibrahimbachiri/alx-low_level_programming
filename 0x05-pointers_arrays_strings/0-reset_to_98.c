2113793055cca99844c3aedae4035987a8f725d7
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-25 23:21:40 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98
+ * @n: Poiter to the integer to be update
+ */
+
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - Updates the value of a integer to 98
+ @n: Pointer to the integer to be updated
+*/
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
