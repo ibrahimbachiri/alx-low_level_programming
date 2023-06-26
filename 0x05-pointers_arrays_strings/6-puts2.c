5bed54b336edcf373806bca5af69046d24647d06
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-26 03:11:23 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, the character written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * puts2 - Prints every other character of a string.
  * @str: The string to be printed.
  */
