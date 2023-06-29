35496edf381743a08494dd84137f3f6ffea3f2ef
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-29 09:46:12 -0700

    103-add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,42 +12,42 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0;
-	int i, j;
+    int len1 = 0, len2 = 0, carry = 0, sum = 0;
+    int i, j;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
+    while (n1[len1] != '\0')
+        len1++;
+    while (n2[len2] != '\0')
+        len2++;
 
-	if (len1 + 1 > size_r || len2 + 1 > size_r)
-		return (0);
+    if (len1 + 1 > size_r || len2 + 1 > size_r)
+        return (0);
 
-	r[size_r - 1] = '\0';
-	i = len1 - 1;
-	j = len2 - 1;
+    r[size_r - 1] = '\0';
+    i = len1 - 1;
+    j = len2 - 1;
 
-	while (i >= 0 || j >= 0)
-	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
+    while (i >= 0 || j >= 0)
+    {
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i] - '0';
+        if (j >= 0)
+            sum += n2[j] - '0';
 
-		carry = sum / 10;
-		r[--size_r] = (sum % 10) + '0';
+        carry = sum / 10;
+        r[--size_r] = (sum % 10) + '0';
 
-		i--;
-		j--;
-	}
+        i--;
+        j--;
+    }
 
-	if (carry != 0)
-	{
-		if (size_r == 1)
-			return (0);
-		r[--size_r] = carry + '0';
-	}
+    if (carry != 0)
+    {
+        if (size_r == 1)
+            return (0);
+        r[--size_r] = carry + '0';
+    }
 
-	return (&r[size_r]);
+    return (&r[size_r]);
 }
