c49c7ef1f0e83b24df11941f2bfed9ac8b8296d4
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-29 06:40:52 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,25 +9,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ptr = dest;
-
-	
+	char *ptr = dest;	
 	while (*ptr != '\0')
 	{
 		ptr++;
 	}
-
-  
 	while (*src != '\0')
 	{
 		*ptr = *src;
 		ptr++;
 		src++;
 	}
-
-	
 	*ptr = '\0';
-
-     
 	return dest;
 }
