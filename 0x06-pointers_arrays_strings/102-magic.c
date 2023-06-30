9857a9e4793619aee5a7c6c924e5ff128cb4eb16
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-06-30 04:05:24 -0700

    commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,9 +9,9 @@ int main(void)
 {
     int a[5] = {98, 198, 298, 398, 498};
     int *p;
-
+    
     p = &a[0];
     *(p + 2) = 98;
-    printf("a[2] = %d\n", *(p + 2));
+    printf("a[2] = %d\n", *(p + 2)); /* Your line of code */
     return 0;
 }
