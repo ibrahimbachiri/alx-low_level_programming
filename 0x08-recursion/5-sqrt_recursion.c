f382285b808075cea31692fc519650faf31c3bc9
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-05 01:01:45 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,13 +11,13 @@ int sqrt_helper(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
+	if (n < 0)
+	  return (-1);
 
-    if (n == 0 || n == 1)
-        return n;
+	if (n == 0 || n == 1)
+	  return (n);
 
-    return sqrt_helper(n, 1);
+	return sqrt_helper(n, 1);
 }
 
 /**
@@ -29,11 +29,11 @@ int _sqrt_recursion(int n)
  */
 int sqrt_helper(int n, int i)
 {
-    if (i * i == n)
-        return i;
+	if (i * i == n)
+	  return (i);
 
-    if (i * i > n)
-        return -1;
+	if (i * i > n)
+	  return (-1);
 
-    return sqrt_helper(n, i + 1);
+	return sqrt_helper(n, i + 1);
 }
