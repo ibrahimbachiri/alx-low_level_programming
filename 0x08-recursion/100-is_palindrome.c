c6846bb5d118a258c9379cac94465eb2b1465db3
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-05 02:34:35 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
+	if (*s == '\0')
+		return (0);
 
-    return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -24,13 +24,13 @@ int _strlen_recursion(char *s)
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
-    if (start >= end)
-        return (1);
+	if (start >= end)
+		return (1);
 
-    if (s[start] != s[end])
-        return (0);
+	if (s[start] != s[end])
+		return (0);
 
-    return (is_palindrome_helper(s, start + 1, end - 1));
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
@@ -41,10 +41,10 @@ int is_palindrome_helper(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-    if (len <= 1)
-        return (1);
+	if (len <= 1)
+		return (1);
 
-    return (is_palindrome_helper(s, 0, len - 1));
+	return (is_palindrome_helper(s, 0, len - 1));
 }
