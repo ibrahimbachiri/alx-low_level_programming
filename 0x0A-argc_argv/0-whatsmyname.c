7f402a060eb4a008b29f30715f0d357e05c9a6e0
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-07 10:00:37 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return 0;
+	(void)argc; /* Silence unused parameter warning */
+
+	printf("%s\n", argv[0]);
+
+	return 0;
 }
