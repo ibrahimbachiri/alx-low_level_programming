e43ee46b17e5065e6c986fa552621e2b49c2e153
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-10 08:55:41 -0700

    m

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,134 +1,29 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * count_words - Counts the number of words in a string.
- * @str: The input string.
- *
- * Return: The number of words.
- */
-int count_words(char *str)
-{
-	int count = 0;
-	int i = 0;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;
+    }
 
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ')
-		{
-			count++;
-			while (str[i] != ' ' && str[i] != '\0')
-				i++;
-		}
-		else
-		{
-			while (str[i] == ' ')
-				i++;
-		}
-	}
+    int cents = atoi(argv[1]);
 
-	return (count);
-}
+    if (cents < 0) {
+        printf("0\n");
+        return 0;
+    }
 
-/**
- * allocate_words - Allocates memory for the words array.
- * @word_count: The number of words.
- *
- * Return: The allocated words array.
- */
-char **allocate_words(int word_count)
-{
-	char **words = (char **)malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
+    int coins = 0;
+    int denominations[] = {25, 10, 5, 2, 1};
+    int num_denominations = sizeof(denominations) / sizeof(denominations[0]);
 
-	return (words);
-}
+    for (int i = 0; i < num_denominations; i++) {
+        coins += cents / denominations[i];
+        cents %= denominations[i];
+    }
 
-/**
- * allocate_word - Allocates memory for a single word.
- * @word_len: The length of the word.
- *
- * Return: The allocated word.
- */
-char *allocate_word(int word_len)
-{
-	char *word = (char *)malloc((word_len + 1) * sizeof(char));
-	if (word == NULL)
-		return (NULL);
+    printf("%d\n", coins);
 
-	return (word);
-}
-
-/**
- * copy_word - Copies a word from the input string to the word array.
- * @word: The word array.
- * @str: The input string.
- * @start: The starting index of the word in the input string.
- * @end: The ending index of the word in the input string.
- */
-void copy_word(char *word, char *str, int start, int end)
-{
-	int i;
-
-	for (i = start; i < end; i++)
-		word[i - start] = str[i];
-
-	word[i - start] = '\0';
-}
-
-/**
- * strtow - Splits a string into words.
- * @str: The input string.
- *
- * Return: An array of strings (words).
- */
-char **strtow(char *str)
-{
-	char **words;
-	int word_count, word_len, i, j, k;
-
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-
-	word_count = count_words(str);
-	if (word_count == 0)
-		return (NULL);
-
-	words = allocate_words(word_count);
-	if (words == NULL)
-		return (NULL);
-
-	i = 0;
-	j = 0;
-	while (str[i] != '\0' && j < word_count)
-	{
-		if (str[i] != ' ')
-		{
-			word_len = 0;
-			k = i;
-			while (str[k] != ' ' && str[k] != '\0')
-			{
-				word_len++;
-				k++;
-			}
-
-			words[j] = allocate_word(word_len);
-			if (words[j] == NULL)
-			{
-				for (k = 0; k < j; k++)
-					free(words[k]);
-				free(words);
-				return (NULL);
-			}
-
-			copy_word(words[j], str, i, k);
-			j++;
-		}
-		i++;
-	}
-
-	words[j] = NULL;
-
-	return (words);
+    return 0;
 }
