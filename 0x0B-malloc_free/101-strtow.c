54654cf57944a6c67865bae55bbab29db2fb8d63
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-10 09:01:31 -0700

    m

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,29 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
+/**
+ * count_words - Counts the number of words in a string
+ * @str: The string to count words in
+ *
+ * Return: The number of words
+ */
+int count_words(char *str)
+{
+    int i, count = 0, len = strlen(str);
+
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+            count++;
     }
 
-    int cents = atoi(argv[1]);
+    return count;
+}
 
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
+/**
+ * strtow - Splits a string into words
+ * @str: The string to split
+ *
+ * Return: Pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+    if (str == NULL || *str == '\0')
+        return NULL;
+
+    int i, j, k, len = strlen(str);
+    int word_count = count_words(str);
+
+    char **words = malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return NULL;
+
+    for (i = 0, j = 0; i < len; i++)
+    {
+        if (str[i] != ' ')
+        {
+            int word_len = 0;
+            for (k = i; k < len && str[k] != ' '; k++)
+                word_len++;
+
+            words[j] = malloc((word_len + 1) * sizeof(char));
+            if (words[j] == NULL)
+            {
+                for (j--; j >= 0; j--)
+                    free(words[j]);
+                free(words);
+                return NULL;
+            }
+
+            strncpy(words[j], str + i, word_len);
+            words[j][word_len] = '\0';
+            i += word_len;
+            j++;
+        }
     }
 
-    int coins = 0;
-    int denominations[] = {25, 10, 5, 2, 1};
-    int num_denominations = sizeof(denominations) / sizeof(denominations[0]);
-
-    for (int i = 0; i < num_denominations; i++) {
-        coins += cents / denominations[i];
-        cents %= denominations[i];
-    }
-
-    printf("%d\n", coins);
-
-    return 0;
+    words[j] = NULL;
+    return words;
 }
