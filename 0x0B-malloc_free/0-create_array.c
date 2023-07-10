dd4fe737d926853d899f703b763cd776c80d7d28
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-09 23:45:27 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    char *array;
+    unsigned int i;
+
+    if (size == 0)
+        return NULL;
+
+    array = (char *)malloc(size * sizeof(char));
+    if (array == NULL)
+        return NULL;
+
+    for (i = 0; i < size; i++)
+        array[i] = c;
+
+    return array;
+}
+
