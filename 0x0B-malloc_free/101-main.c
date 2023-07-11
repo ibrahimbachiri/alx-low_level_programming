b45bcc1e171a1cd0ae2a4beac9b51a75dedd8991
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-11 09:37:02 -0700

    m

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -2,27 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * print_tab - Prints an array of string
- * @tab: The array to print
- *
- * Return: nothing
- */
 void print_tab(char **tab)
 {
     int i;
 
     for (i = 0; tab[i] != NULL; ++i)
     {
-        printf("%s\n", tab[i]);
+        printf("%s$\n", tab[i]);
     }
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
 int main(void)
 {
     char **tab;
@@ -31,8 +20,11 @@ int main(void)
     if (tab == NULL)
     {
         printf("Failed\n");
-        return (1);
+        return 1;
     }
     print_tab(tab);
-    return (0);
+
+    free(tab);
+
+    return 0;
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,68 +1,68 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * count_words - Counts the number of words in a string
- * @str: The string to count words in
- *
- * Return: The number of words
- */
-int count_words(char *str)
+char **strtow(char *str)
 {
-    int i, count = 0, len = strlen(str);
+    int len = 0, l = 0, i, j = 0;
+    char **s;
+
+    if (str == NULL)
+        return NULL;
+
+    for (len = 0; str[len] != '\0'; len++)
+        ;
+
+    s = (char **)malloc((len + 1) * sizeof(char *));
+    if (s == NULL)
+        return NULL;
 
     for (i = 0; i < len; i++)
     {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-            count++;
-    }
-
-    return count;
-}
-
-/**
- * strtow - Splits a string into words
- * @str: The string to split
- *
- * Return: Pointer to an array of strings (words)
- */
-char **strtow(char *str)
-{
-    if (str == NULL || *str == '\0')
-        return NULL;
-
-    int i, j, k, len = strlen(str);
-    int word_count = count_words(str);
-
-    char **words = malloc((word_count + 1) * sizeof(char *));
-    if (words == NULL)
-        return NULL;
-
-    for (i = 0, j = 0; i < len; i++)
-    {
-        if (str[i] != ' ')
+        if (str[i] == ' ' && l == 0)
         {
-            int word_len = 0;
-            for (k = i; k < len && str[k] != ' '; k++)
-                word_len++;
-
-            words[j] = malloc((word_len + 1) * sizeof(char));
-            if (words[j] == NULL)
-            {
-                for (j--; j >= 0; j--)
-                    free(words[j]);
-                free(words);
-                return NULL;
-            }
-
-            strncpy(words[j], str + i, word_len);
-            words[j][word_len] = '\0';
-            i += word_len;
+            s[j] = malloc((i - l + 1) * sizeof(char));
+            strncpy(s[j], str + l, i - l);
+            s[j][i - l] = '\0';
             j++;
+            l = i + 1;
+        }
+        else if (str[i] != ' ')
+        {
+            if (l == 1)
+            {
+                l = i;
+            }
+            else if (l == 0 && (i == 0 || str[i - 1] == ' '))
+            {
+                l = i;
+            }
         }
     }
 
-    words[j] = NULL;
-    return words;
+    if (l < len)
+    {
+        s[j] = malloc((len - l + 1) * sizeof(char));
+        strncpy(s[j], str + l, len - l);
+        s[j][len - l] = '\0';
+        j++;
+    }
+
+    s[j] = NULL;
+
+    return s;
+}
+
+void free_tab(char **tab)
+{
+    int i;
+
+    if (tab == NULL)
+        return;
+
+    for (i = 0; tab[i] != NULL; i++)
+    {
+        free(tab[i]);
+    }
+    free(tab);
 }
