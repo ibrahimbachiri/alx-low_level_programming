bb97b73e82a1e8c53b364bd13881dc375cd4232e
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-17 03:49:27 -0700

    Macro-Name

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,9 @@
+/*
+ * File: 0-object_like_macro.h
+ * Author: [Your Name]
+ * Desc: Header file that defines a macro named BUFFER_SIZE as an abbreviation
+ *       for the token 1024.
+ */
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 #define BUFFER_SIZE 1024
