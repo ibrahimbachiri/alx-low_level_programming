1b9424bdacad774c1d9164ecb2ad6a5a9ca77747
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-17 08:36:37 -0700

    m

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program.
+ * main - Entry point of the program
  *
- * Description: This function is the starting point of the program. It prints
- *              the name of the file it was compiled from, followed by a new line.
+ * Description: The main function serves as the entry point of the program. It
+ * prints the name of the file it was compiled from, followed by a new line.
  *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
