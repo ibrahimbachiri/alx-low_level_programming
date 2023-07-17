9ab24a4f413ffb3e02b1026f251731247236832d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-17 06:01:27 -0700

    Poppy

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated for a dog_t structure.
+ * @d: Pointer to the dog_t structure to be freed.
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+
+    /* Free the name and owner strings */
+    free(d->name);
+    free(d->owner);
+
+    /* Free the dog_t structure itself */
+    free(d);
+}
