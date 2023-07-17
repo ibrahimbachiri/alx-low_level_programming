315e70500ec3977c09c7771078a74106d2789b7e
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-17 08:53:10 -0700

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+
 /**
  * print_dog - Prints the details of a struct dog.
  * @d: Pointer to the struct dog to be printed.
@@ -10,6 +11,7 @@ void print_dog(struct dog *d)
 {
     if (d == NULL)
         return;
+    
     printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
     printf("Age: %.6f\n", d->age);
     printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
