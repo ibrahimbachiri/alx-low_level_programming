3ae7b659f11fffdf4f73585852e0dd9b5f84d191
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-17 06:09:12 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* Define the struct dog type */
 typedef struct dog {
     char *name;
     float age;
     char *owner;
 } dog_t;
 
-/* Function prototypes */
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
