ca1a190b050eefc7866fe7f44344de0138acabfb
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-18 00:25:33 -0700

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,41 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog.
- * @name: Pointer to the string representing the dog's name.
- * @age: The age of the dog.
- * @owner: Pointer to the string representing the owner's name.
+ * _strdup - Duplicates a string.
+ * @str: The string to be duplicated.
  *
- * Return: Pointer to the newly created dog_t structure.
+ * Return: A pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+	char *dup_str;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	dup_str = malloc(sizeof(char) * (len + 1));
+
+	if (dup_str == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dup_str[i] = str[i];
+
+	return (dup_str);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the new dog (dog_t).
  *         NULL if the function fails.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    if (name == NULL || owner == NULL)
-        return (NULL);
-    /* Allocate memory for the dog_t structure */
-    new_dog = (dog_t *)malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-    /* Allocate memory for the name and owner strings */
-    new_dog->name = strdup(name);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    new_dog->owner = strdup(owner);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-    /* Assign the age */
-    new_dog->age = age;
-    return (new_dog);
+	dog_t *new_dog_ptr;
+
+	new_dog_ptr = malloc(sizeof(dog_t));
+
+	if (new_dog_ptr == NULL)
+		return (NULL);
+
+	new_dog_ptr->name = _strdup(name);
+	if (new_dog_ptr->name == NULL)
+	{
+		free(new_dog_ptr);
+		return (NULL);
+	}
+
+	new_dog_ptr->owner = _strdup(owner);
+	if (new_dog_ptr->owner == NULL)
+	{
+		free(new_dog_ptr->name);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+
+	new_dog_ptr->age = age;
+
+	return (new_dog_ptr);
 }
