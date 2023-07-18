8d72e84ef22e2e65623257777350e8edb03a0f90
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-17 23:32:04 -0700

    m

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
 
-#define BUFFER_SIZE	1024
+#define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO_H */
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */
