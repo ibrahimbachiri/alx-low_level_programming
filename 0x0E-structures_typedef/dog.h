fcab10fdad324b64ad164791500b5d4d6928e4d4
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-18 00:12:31 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-
+/* Function prototype for init_dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#endif
+#endif /* DOG_H */
