aa6e63f7fc0ee42706a6d6c8861109b13ce2a6a7
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 07:37:56 -0700

    b

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-/**
+/*
  * struct dog - A simple structure for a dog's information.
  * @name: The name of the dog.
  * @age: The age of the dog.
@@ -13,14 +13,14 @@
  *              including its name, age, and owner.
  */
 
-/**
+/*
  * print_name - Prints a name using the given printing function.
  * @name: Pointer to the name to be printed.
  * @f: Pointer to the printing function.
  */
 void print_name(char *name, void (*f)(char *));
 
-/**
+/*
  * array_iterator - Executes a function on each element of an array.
  * @array: Pointer to the array of integers.
  * @size: The size of the array.
