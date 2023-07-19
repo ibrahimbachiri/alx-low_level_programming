912672d86fa0995267e31e8483c349bcdf694a64
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 09:59:23 -0700

    m

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
 
 /* Function that prints a name as is */
 void print_name(char *name, void (*f)(char *));
-
-/* Function that iterates over an array and performs an action on each element */
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 #endif /* FUNCTION_POINTERS_H */
