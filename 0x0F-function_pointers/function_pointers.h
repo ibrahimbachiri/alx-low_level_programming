375be0432c0558ff3efe27f3794fb423e3e5e120
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 07:16:17 -0700

    m

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*
+/**
  * Description: This header file contains function prototypes for function_pointer.c.
  * - print_name: Takes a name and a function pointer as arguments and calls the function
- *               pointed by the function pointer to print the name in a specific format.
+ * pointed by the function pointer to print the name in a specific format.
  */
 void print_name(char *name, void (*f)(char *));
+/* Function prototype for print_name */
 #endif /* FUNCTION_POINTERS_H */
