f6050b69ab3f4609aacb3ed8f8214c37149fba66
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 07:35:09 -0700

    b

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,31 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*
- * Description: This header file contains function prototypes for function_pointer.c.
- * - print_name: Takes a name and a function pointer as arguments and calls the function
- * pointed by the function pointer to print the name in a specific format.
+#include <stddef.h>
+
+/**
+ * struct dog - A simple structure for a dog's information.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This structure represents basic information about a dog,
+ *              including its name, age, and owner.
+ */
+
+/**
+ * print_name - Prints a name using the given printing function.
+ * @name: Pointer to the name to be printed.
+ * @f: Pointer to the printing function.
  */
 void print_name(char *name, void (*f)(char *));
-/* Function prototype for print_name */
+
+/**
+ * array_iterator - Executes a function on each element of an array.
+ * @array: Pointer to the array of integers.
+ * @size: The size of the array.
+ * @action: Pointer to the function to be executed on each array element.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif /* FUNCTION_POINTERS_H */
