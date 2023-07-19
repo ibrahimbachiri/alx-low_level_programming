e7cb535e9d1276d4df52974aea819ec30cecf4f8
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 07:01:48 -0700

    m

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,14 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/**
- * struct dog - A simple structure for a dog's information.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+/*
+ * Description: This header file contains function prototypes for function_pointer.c.
+ * It declares three functions:
+ * - print_name: Takes a name and a function pointer as arguments and calls the function
+ *               pointed by the function pointer to print the name in a specific format.
+ * - print_name_as_is: Prints a given name as it is without any modifications.
+ * - print_name_uppercase: Prints a given name in uppercase letters.
  */
 void print_name(char *name, void (*f)(char *));
-#endif
+/* Function prototype for print_name */
+#endif /* FUNCTION_POINTERS_H */
