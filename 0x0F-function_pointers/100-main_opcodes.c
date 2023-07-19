269060009ca1ac977ac5b1e0c59ad41d68f382cd
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 09:26:56 -0700

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - Entry point of the program
  * @argc: Number of arguments passed to the program
  * @argv: Array of strings containing the arguments
