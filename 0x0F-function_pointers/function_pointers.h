84bcb5131df9429e4bc274ff27f59542d60a5d4d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 10:23:04 -0700

    m

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+
+/* Prints a name using the given printing function. */
 void print_name(char *name, void (*f)(char *));
-/*                                                                                                                                                   * array_iterator - Executes a function on each element of an array.                                                                                 * @array: Pointer to the array of integers.                                                                                                         * @size: The size of the array.                                                                                                                     * @action: Pointer to the function to be executed on each array element.                                                                            */
+
+/* Executes a function on each element of an array. */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* Searches for an integer in an array. */
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* FUNCTION_POINTERS_H */
