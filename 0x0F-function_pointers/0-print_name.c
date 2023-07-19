b91e01e589e21a0ad3ee3024e3f794dafec28e90
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 10:11:11 -0700

    m

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-/**
+/*
  * print_name - Prints a name using the given printing function.
  * @name: Pointer to the name to be printed.
  * @f: Pointer to the printing function.
