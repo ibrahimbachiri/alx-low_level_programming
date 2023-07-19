7856d4b3246ef13cf2d72392f179bd3e80df97b4
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-19 06:36:11 -0700

    m

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,17 +4,7 @@
 /**
  * Description: This header file contains function prototypes for function_pointer.c.
  * It declares three functions:
- * - print_name: Takes a name and a function pointer as arguments and calls the function
- *               pointed by the function pointer to print the name in a specific format.
- * - print_name_as_is: Prints a given name as it is without any modifications.
- * - print_name_uppercase: Prints a given name in uppercase letters.
  */
-
-/* Function prototype for print_name */
 void print_name(char *name, void (*f)(char *));
-
-/* Function prototypes for print_name_as_is and print_name_uppercase */
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
 
 #endif /* FUNCTION_POINTERS_H */
