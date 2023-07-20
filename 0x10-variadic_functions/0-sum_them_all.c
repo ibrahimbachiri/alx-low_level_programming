919460ffd86572201f74ea4a8644f29c656c9435
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-20 08:47:49 -0700

    d

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 	{
 		return (0);
-	}	
+	}
 
 	va_start(args, n);
 
@@ -26,5 +26,6 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(args, int);
 	}
 	va_end(args);
+
 	return (sum);
 }
