e4e80927b6abef440d3875aa75cce8182a4adc7e
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-20 02:03:04 -0700

    m

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * struct format - Struct to store a format specifier and its corresponding function
+ * @spec: The format specifier
+ * @func: The corresponding function to handle the format specifier
+ */
+typedef struct format
+{
+	char *spec;
+	void (*func)(va_list);
+} format_t;
+
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif /* VARIADIC_FUNCTIONS_H */
