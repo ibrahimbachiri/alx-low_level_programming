bf9c943f6a692948c6cf984a8ae5b18801da1d64
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-20 08:26:54 -0700

    d

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array && action)
-    {
-        size_t i;
-        for (i = 0; i < size; i++)
-            action(array[i]);
-    }
+	if (array && action)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
