5b6d3bfbfb659ba3a23dd66aafa145438a975779
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-20 02:10:41 -0700

    m

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,6 +13,4 @@ typedef struct format
 } format_t;
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-
 #endif /* VARIADIC_FUNCTIONS_H */
