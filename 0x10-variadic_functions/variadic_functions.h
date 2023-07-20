b7aabbea97bf66f86801475bad882800a7ff2f46
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-20 06:06:59 -0700

    d

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,14 +8,16 @@
  * @spec: The format specifier
  * @func: The corresponding function to handle the format specifier
  */
-typedef struct format
+struct format
 {
 	char *spec;
 	void (*func)(va_list args);
-}format_t;
+};
+typedef struct format format_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif /* VARIADIC_FUNCTIONS_H */
