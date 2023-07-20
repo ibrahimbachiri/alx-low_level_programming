e4e892a345a1e8abeaf16457bf347f3e4024aa00
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-20 08:31:32 -0700

    d

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array && cmp)
-	{
-		size_t i;
-		for (i = 0; i < (size_t)size; i++)
-		{
-			if (cmp(array[i]))
-				return (i);
-		}
-	}
-	return (-1);
+    if (array && cmp)
+    {
+        size_t i;
+
+        for (i = 0; i < (size_t)size; i++)
+        {
+            if (cmp(array[i]))
+                return (i);
+        }
+    }
+    return (-1);
 }
