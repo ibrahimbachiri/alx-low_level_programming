a6bc58e150dec9b88a94b1889b7c0441a48d82fc
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-21 08:10:12 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,47 +3,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - Prints a char argument
- * @args: va_list containing the arguments
- */
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - Prints an int argument
- * @args: va_list containing the arguments
- */
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - Prints a float argument
- * @args: va_list containing the arguments
- */
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - Prints a string argument
- * @args: va_list containing the arguments
- */
-void print_string(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	if (str == NULL)
-		str = "(nil)";
-
-	printf("%s", str);
-}
-
-/**
  * print_all - Prints anything based on the format provided
  * @format: A list of types of arguments passed to the function
  * c: char
