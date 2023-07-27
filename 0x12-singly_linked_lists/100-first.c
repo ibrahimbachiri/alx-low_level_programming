146b13eb9ded2f5d653e347eaa3ccd84c66a84fa
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-27 05:39:11 -0700

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-void print_before_main(void) __attribute__((constructor));
+void print_before_main(void) __attribute__ ((constructor));
 
 /**
- * print_before_main - Prints the desired text before the main function is executed.
+ * print_before_main - Function to be executed before the main function
  */
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	char *msg = "You're beat! and yet, you must allow,\n"
+		    "I bore my house upon my back!\n";
+
+	printf("%s", msg);
 }
