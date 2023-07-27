b3ea8fa008f92fdfa594fb3a9da4fe8de052f75b
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-27 07:22:49 -0700

    mm

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,4 @@
-k#include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -11,36 +11,36 @@ k#include <stdlib.h>
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
+    if (str == NULL)
+        return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+    list_t *new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	new_node->len = strlen(str);
-	new_node->next = NULL;
+    new_node->len = strlen(str);
+    new_node->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		list_t *current = *head;
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new_node;
-	}
+    if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        list_t *current = *head;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
 
-	return (new_node);
+    return (new_node);
 }
