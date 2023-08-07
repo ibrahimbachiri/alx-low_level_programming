eba4edee8f98f3fc3a53fd0b472bb38473ce4580
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-08-07 07:41:15 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,37 +15,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file_descriptor, bytes_read, bytes_written;
-    char *buffer;
+	ssize_t file_descriptor, bytes_read, bytes_written;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-    buffer = (char *)malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(file_descriptor);
-        return (0);
-    }
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    free(buffer);
-    close(file_descriptor);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	free(buffer);
+	close(file_descriptor);
 
-    if (bytes_written == -1 || bytes_written != bytes_read)
-        return (0);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+		return (0);
 
-    return (bytes_written);
+	return (bytes_written);
 }
