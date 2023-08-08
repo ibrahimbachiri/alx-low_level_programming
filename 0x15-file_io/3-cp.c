d56b816876a8b9ee8fecc6bdab1085d8c367b277
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-08-08 08:52:13 -0700

    m

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,55 +14,55 @@
  */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to, n_read, n_written;
-    char buffer[BUF_SIZE];
+	int fd_from, fd_to, n_read, n_written;
+	char buffer[BUF_SIZE];
 
-    /* Check the number of arguments. */
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-        exit(97);
-    }
+	/* Check the number of arguments. */
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
 
-    /* Open the source file for reading. */
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	/* Open the source file for reading. */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    /* Open the destination file for writing, create it if it doesn't exist. */
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-        exit(99);
-    }
+	/* Open the destination file for writing, create it if it doesn't exist. */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
 
-    /* Read from the source file and write to the destination file. */
-    while ((n_read = read(fd_from, buffer, BUF_SIZE)) > 0)
-    {
-        n_written = write(fd_to, buffer, n_read);
-        if (n_written == -1 || n_written != n_read)
-        {
-            dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-            exit(99);
-        }
-    }
+	/* Read from the source file and write to the destination file. */
+	while ((n_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written == -1 || n_written != n_read)
+		{
+			dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+			exit(99);
+		}
+	}
 
-    if (n_read == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	if (n_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    /* Close the file descriptors. */
-    if (close(fd_from) == -1 || close(fd_to) == -1)
-    {
-        dprintf(2, "Error: Can't close files\n");
-        exit(100);
-    }
+	/* Close the file descriptors. */
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close files\n");
+		exit(100);
+	}
 
-    return (0);
+	return (0);
 }
