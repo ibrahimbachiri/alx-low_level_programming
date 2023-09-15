f415a7830b4b966742318574977cc9ed0705629c
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-15 02:56:49 -0700

    double

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,10 +11,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head;
 	unsigned int i = 0;
-
 	if (!*head)
 	return (-1);
-
 	if (index == 0)
 	{
 	*head = (*head)->next;
@@ -23,21 +21,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	free(current);
 	return (1);
 	}
-
 	while (current && i < index)
 	{
 	current = current->next;
 	i++;
 	}
-
 	if (!current)
 	return (-1);
-
 	if (current->next)
         current->next->prev = current->prev;
 	if (current->prev)
 	current->prev->next = current->next;
-
 	free(current);
 	return (1);
 }
