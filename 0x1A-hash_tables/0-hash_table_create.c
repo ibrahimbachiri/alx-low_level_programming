3755a5730cc69d8f9b4bc13516c36b14a7a1a0d4
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-05 16:20:20 -0700

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,8 +8,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table;
-  unsigned long int i;
+	hash_table_t *new_table;
+	unsigned long int i;
   
   if (size == 0)
     return (NULL);
@@ -18,16 +18,14 @@ hash_table_t *hash_table_create(unsigned long int size)
   
   if (new_table == NULL)
     return (NULL);
-  
   new_table->size = size;
   new_table->array = malloc(sizeof(hash_node_t *) * size);
-  
   if (new_table->array == NULL)
-    {
-      free(new_table);
-      return (NULL);
-    }
+  {
+	  free(new_table);
+	  return (NULL);
+  }
   for (i = 0; i < size; i++)
-    new_table->array[i] = NULL;
+	  new_table->array[i] = NULL;
   return (new_table);
 }
