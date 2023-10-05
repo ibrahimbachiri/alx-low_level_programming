4dea4ef178bbe6bf912f757811ce459efab9ec5d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-05 16:16:03 -0700

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
   hash_table_t *new_table;
   unsigned long int i;
+  
   if (size == 0)
     return (NULL);
+  
   new_table = malloc(sizeof(hash_table_t));
+  
   if (new_table == NULL)
     return (NULL);
+  
   new_table->size = size;
   new_table->array = malloc(sizeof(hash_node_t *) * size);
+  
   if (new_table->array == NULL)
     {
       free(new_table);
