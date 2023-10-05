6e323ffeae6459a34e603b8f8c1cea38f63b65c9
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-05 08:32:28 -0700

    m

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "hash_tables.h"
 /**
  * hash_djb2 - Hash function using the DJB2 algorithm
  * @str: The string to hash
  *
- * Return: The hash value generated from the input string
+ * Return: The index in the has table.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
